a7a2e59801f9981e948035965b0f7fe06f1f0749
stalker474 <anton.possylkine@gmail.com>
2021-04-06 12:54:19 +0200

    updated contracts

--- src/components/Blockchain/Updater.js
+++ src/components/Blockchain/Updater.js
@@ -20,24 +20,27 @@ const erc20Abi = require("../../contracts/ERC20.json");
 const erc721Abi = require("../../contracts/ERC721.json");
 const AmplesenseVaultAbi = require("../../contracts/AmplesenseVault.json");
 const StakingERC20Abi = require("../../contracts/StakingERC20.json");
+const StakingERC721Abi = require("../../contracts/StakingERC721.json");
 
 const axios = require('axios')
 
 //define variables for contract addresses
 export const CONTRACT_ADDRESSES = {
-  AMPLE_SENSE_VAULT: '0xDc64a140Aa3E981100a9becA4E685f962f0cF6C9',
+  AMPLE_SENSE_VAULT: '0x5FC8d32690cc91D4c39d9d3abcBD16989F875707',
   AMPLE_CONTRACT: '0x5FbDB2315678afecb367f032d93F642f64180aa3',
   KMPL_CONTRACT: '0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512',
-  PIONEER1_CONTRACT: '0x5FC8d32690cc91D4c39d9d3abcBD16989F875707',
-  PIONEER2_CONTRACT: '0x0165878A594ca255338adfa4d48449f69242Eb8F',
-  LPSTAKING_CONTRACT: '0xa513E6E4b8f2a923D98304ec87F64353C4D5C853',
-  NFT_CONTRACT: '0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0',
+  PIONEER1_CONTRACT: '0x0165878A594ca255338adfa4d48449f69242Eb8F',
+  PIONEER2_CONTRACT: '0xa513E6E4b8f2a923D98304ec87F64353C4D5C853',
+  LPSTAKING_CONTRACT: '0x2279B7A0a67DB372996a5FaB50D91eAA73d2eBe6',
+  NFT1_CONTRACT: '0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0',
+  NFT2_CONTRACT: '0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9',
   Univ3_EEFI_ETH_CONTRACT: ''
 }
 
 export const VaultType = {
   AMPLESENSE : {vault: CONTRACT_ADDRESSES.AMPLE_SENSE_VAULT, staking_token: CONTRACT_ADDRESSES.AMPLE_CONTRACT, vault_abi: AmplesenseVaultAbi, staking_token_abi: erc20Abi, staking_symbol: "AMPL", name: "Elastic Vault: AMPL > EEFI"},
-  PIONEER1 : {vault: CONTRACT_ADDRESSES.PIONEER1_CONTRACT, staking_token: CONTRACT_ADDRESSES.NFT_CONTRACT, vault_abi: StakingERC20Abi, staking_token_abi: erc721Abi, staking_symbol: "kMPL NFT", name: "Pioneer Fund Vault I: NFT"},
+  PIONEER1A : {vault: CONTRACT_ADDRESSES.PIONEER1_CONTRACT, staking_token: CONTRACT_ADDRESSES.NFT1_CONTRACT, vault_abi: StakingERC721Abi, staking_token_abi: erc721Abi, staking_symbol: "NFT", name: "Pioneer Fund Vault I: NFT"},
+  PIONEER1B : {vault: CONTRACT_ADDRESSES.PIONEER1_CONTRACT, staking_token: CONTRACT_ADDRESSES.NFT2_CONTRACT, vault_abi: StakingERC721Abi, staking_token_abi: erc721Abi, staking_symbol: "NFT", name: "Pioneer Fund Vault I: NFT"},
   PIONEER2 : {vault: CONTRACT_ADDRESSES.PIONEER2_CONTRACT, staking_token: CONTRACT_ADDRESSES.KMPL_CONTRACT, vault_abi: StakingERC20Abi, staking_token_abi: erc20Abi, staking_symbol: "kMPL", name: "Pioneer Fund Vault II: kMPL"},
   LPSTAKING : {vault: CONTRACT_ADDRESSES.LPSTAKING_CONTRACT, staking_token: CONTRACT_ADDRESSES.Univ3_EEFI_ETH_CONTRACT, vault_abi: StakingERC20Abi, staking_token_abi: erc20Abi, staking_symbol: "UniswapV2", name: "EEFI/ETH LP Token Vault"}
 }
@@ -61,7 +64,7 @@ export class VaultContract {
 
   allowance() {
     const contract = new this.state.web3.eth.Contract(this.state.type.staking_token_abi.abi, this.state.type.staking_token);
-    if(this.state.type == VaultType.PIONEER1) {
+    if(this.state.type == VaultType.PIONEER1A || this.state.type == VaultType.PIONEER1B) {
       return contract.methods.isApprovedForAll(this.state.account, CONTRACT_ADDRESSES.PIONEER1).call();
     } else {
       return contract.methods.allowance(this.state.account, this.state.type.vault).call();
@@ -98,7 +101,7 @@ export class VaultContract {
 
   approve(amount) {
     const contract = new this.state.web3.eth.Contract(this.state.type.staking_token_abi.abi, this.state.type.staking_token);
-    if(this.state.type == VaultType.PIONEER1) {
+    if(this.state.type == VaultType.PIONEER1A || this.state.type == VaultType.PIONEER1B) {
       return contract.methods.setApprovalForAll(CONTRACT_ADDRESSES.PIONEER1, true).send({from: this.state.account});
     } else {
       return contract.methods.approve(this.state.type.vault, amount.toString()).send({from: this.state.account});
@@ -109,7 +112,10 @@ export class VaultContract {
     const contract = new this.state.web3.eth.Contract(this.state.type.vault_abi.abi, this.state.type.vault);
     if(this.state.type == VaultType.AMPLESENSE) {
       return contract.methods.makeDeposit(amount.toString()).send({from: this.state.account});
-    } else {
+    } else if(this.state.type == VaultType.PIONEER1A || this.state.type == VaultType.PIONEER1B) {
+      return contract.methods.stake(amount.toString(), this.state.type.staking_token).send({from: this.state.account});
+    }
+    else {
       return contract.methods.stake(amount.toString(), "0x").send({from: this.state.account});
     }
   }
@@ -118,7 +124,10 @@ export class VaultContract {
     const contract = new this.state.web3.eth.Contract(this.state.type.vault_abi.abi, this.state.type.vault);
     if(this.state.type == VaultType.AMPLESENSE) {
       return contract.methods.withdraw(amount.toString()).send({from: this.state.account});
-    } else {
+    } else if(this.state.type == VaultType.PIONEER1A || this.state.type == VaultType.PIONEER1B) {
+      return contract.methods.unstake(amount.toString(), this.state.type.staking_token).send({from: this.state.account});
+    }
+     else {
       return contract.methods.unstake(amount.toString(), "0x").send({from: this.state.account});
     }
   }
